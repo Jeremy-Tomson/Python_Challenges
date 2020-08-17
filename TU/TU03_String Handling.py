cf2ac89b1d03bdab5e1817a753dc741a75b658d7
Jeremy-Tomson <48037011+Jeremy-Tomson@users.noreply.github.com>
2020-08-17 11:27:36 +0800

    Delete TU03_String Handling.py

--- TU/TU03_String Handling.py
+++
@@ -1,9 +0,0 @@
-#Exercise 1
-MyAge = int(input('What is your age?'))
-print('I am younger than %d' % MyAge)
-print(MyAge, "is a very nice age to be?")
-
-#Exercise 2
-ComputerAge=3.444
-MyAge = int(input('What is your age?'))
-print('I am older than %d, because I am %d' % (MyAge,ComputerAge))
