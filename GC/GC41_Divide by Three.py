b5b16f3c47fb5086de1f9abf6d2d4e1284150986
Jeremy-Tomson <48037011+Jeremy-Tomson@users.noreply.github.com>
2020-08-17 11:30:15 +0800

    Delete GC41_Divide by Three.py

--- GC/GC41_Divide by Three.py
+++
@@ -1,4 +0,0 @@
-numbym = int(input("If you would like to know if a number is divisible by three, put in the number."))
-numby3 = divmod(numbym, 3)
-print (numby3)
-print ("The number on the right is the remaninder if the number cannot be divisible, if it is divisible by three a zero will be displayed.")
