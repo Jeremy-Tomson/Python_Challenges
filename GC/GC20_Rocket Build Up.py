11ed4fc42802b5f97fce403dce0861b3c3797cb4
Jeremy-Tomson <48037011+Jeremy-Tomson@users.noreply.github.com>
2020-08-17 11:30:07 +0800

    Delete GC20_Rocket Build Up.py

--- GC/GC20_Rocket Build Up.py
+++
@@ -1,9 +0,0 @@
-Hours = 51
-Days = 0
-while Hours > 24:
-    Hours = Hours - 24
-    days = Days +1
-    if Hours < 24:
-       FinalTime = 2 + Hours
-       print("The time that the rocket will be ready is at %s pm." % FinalTime)
-       print(Days)
